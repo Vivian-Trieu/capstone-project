d67c3df29e0a8c0ff5f745ff2409bcd1e3da4a31
Vivian Trieu <vivian.dtrieu@gmail.com>
2024-07-09 14:07:23 -0700

    added and organized css files

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,26 +3,14 @@ import Logo from '../images/Logo.svg';
 const Nav = () => {
     return (
         <nav>
-            <img src={Logo} alt='Logo'></img>
             <ul>
-                <li>
-                    <a href='/Home'>Home</a>
-                </li>
-                <li>
-                    <a href='/About'>About</a>
-                </li>
-                <li>
-                    <a href='/Menu'>Menu</a>
-                </li>
-                <li>
-                    <a href='/Reservations'>Reservations</a>
-                </li>
-                <li>
-                    <a href='/Order-Online'>Order Online</a>
-                </li>
-                <li>
-                    <a href='/Login'>Login</a>
-                </li>
+                <li><img src={Logo} alt='Logo'></img></li>
+                <li><a href='#home'>Home</a></li>
+                <li><a href='#about'>About</a></li>
+                <li><a href='#menu'>Menu</a></li>
+                <li><a href='#reservations'>Reservations</a></li>
+                <li><a href='#order-online'>Order Online</a></li>
+                <li><a href='#login'>Login</a></li>
             </ul>
         </nav>
     )
