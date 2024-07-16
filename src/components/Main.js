04c310929332c75d940b3f8af1ab24daab50721f
Vivian Trieu <vivian.dtrieu@gmail.com>
2024-07-16 15:11:59 -0700

    added rest of the specials

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,30 @@
 import Card from './Card';
 import GreekSalad from '../images/greek_salad.jpg';
+import Bruschetta from '../images/bruschetta.jpg';
+import LemonDessert from '../images/lemon_dessert.jpg';
+
+const specials = [
+    {
+        title: 'Greek Salad',
+        price: '$12.99',
+        description: "The famous greek salad of crispy lettuce, peppers, olives, and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.",
+        imageSrc: GreekSalad,
+    },
+    {
+        title: 'Bruschetta',
+        price: '$5.99',
+        description: "Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil.",
+        imageSrc: Bruschetta,
+    },
+    {
+        title: 'Lemon Dessert',
+        price: '$5.99',
+        description: "Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil.",
+        imageSrc: LemonDessert,
+    }
+];
 
 const Main = () => {
-    const specials = [
-        {
-            title: 'Greek Salad',
-            price: '$12.99',
-            description: "The famous greek salad of crispy lettuce, peppers, olives, and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.",
-            imageSrc: GreekSalad,
-        }
-    ];
-
     return (
         <main>
             <section class='specials'>
@@ -18,13 +32,13 @@ const Main = () => {
                     <h1>This week's specials</h1>
                     <button>Online Menu</button>
                 </div>
-                {specials.map ((special) => (
+                {specials.map((special) => (
                     <Card
                         key={special.title}
                         title={special.title}
                         price={special.price}
                         description={special.description}
-                        getImageSrc={special.imageSrc}
+                        imageSrc={special.imageSrc}
                     />
                 ))}
             </section>
