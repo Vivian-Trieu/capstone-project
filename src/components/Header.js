a5aaf4f94b5171f1929651f4f2a4a216dbd36c2d
Vivian Trieu <vivian.dtrieu@gmail.com>
2024-07-16 12:21:15 -0700

    added and styled header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,21 @@
+import Food from '../images/restaurantfood.jpg';
+
 const Header = () => {
     return (
         <header>
-            <h1>Little Lemon</h1>
-            <h2>Chicago</h2>
-            
+            <div class='content'>
+                <div class='callToAction'>
+                    <h1>Little Lemon</h1>
+                    <h2>Chicago</h2>
+                    <h6>
+                        We are a family owned Mediterranean restaurant,
+                        focused on traditional recipes served with a modern
+                        twist.
+                    </h6>
+                    <button>Reserve a Table</button>
+                </div>
+                <img src={Food} alt="Food"></img>
+            </div>
         </header>
     )
 }
