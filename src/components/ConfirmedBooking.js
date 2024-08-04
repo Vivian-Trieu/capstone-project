cf22759151da1508d10e3edcfab991ae0b177f4b
Vivian Trieu <vivian.dtrieu@gmail.com>
2024-08-03 23:28:54 -0700

    styled confirmed booking page

--- src/components/ConfirmedBooking.js
+++ src/components/ConfirmedBooking.js
@@ -1,9 +1,29 @@
+import Nav from './Nav';
+import Footer from './Footer';
+import '../styles/confirmedbooking.css';
+import { useNavigate } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck } from '@fortawesome/free-solid-svg-icons'
+
 const ConfirmedBooking = () => {
+    const navigate = useNavigate();
+
+    const handleButtonClick = () => {
+        navigate('/');
+    }
+
+
     return (
-        <div>
-            <h1>Booking Confirmed!</h1>
-            <p>Your booking has been successfully confirmed. Thank you for choosing Little Lemon!</p>
-        </div>
+        <>
+            <Nav />
+            <div className="confirmation-container">
+                <FontAwesomeIcon className="icon" icon={faCheck} size="5x"/>
+                <h1>Booking Confirmed!</h1>
+                <h6>Your booking has been successfully confirmed.</h6>
+                <h6>Thank you for choosing Little Lemon!</h6>
+                <button onClick={handleButtonClick}>Back to Home</button>
+            </div>
+        </>
     )
 }
 
